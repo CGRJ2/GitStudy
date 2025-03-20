60f8de683b155a672ba8de066b2662971efcf699
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-03-20 12:43:12 +0900

    commit test

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -10,7 +10,12 @@ namespace GitStudy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dsfasdf");
+            
+            Console.WriteLine("1차 변경");
+            Console.WriteLine("커밋 변경 고고");
+            Console.WriteLine("오케이");
+            Console.WriteLine("휘비고");
+            
         }
     }
 }
